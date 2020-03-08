ed7c9452927d3de61bd1f26bfadc4e11e324e394
cjber <cjberragan@gmail.com>
2020-03-08 16:21:13 +0000

    daily

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,7 +1,12 @@
 fish_vi_key_bindings
-
-# custom theme
 set fish_greeting
+
+# Set the cursor shapes for the different vi modes.
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
+
 
 # alias
 alias v="nvim"
@@ -12,6 +17,7 @@ alias la="ncdu"
 alias rm="trashf"
 alias empty='echo -n Emptying the Bin | pv -qL 10 && command rm -rf ~/.local/share/Trash/files/*'
 thefuck --alias | source
+alias conf="cd ~/dotfiles/dotfiles/"
 
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
     if test "$argv" = !!
