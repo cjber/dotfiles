32a889e0c2b20841986819fcf7177a53ee1f0beb
cjber <cjberragan@gmail.com>
2020-03-21 13:32:15 +0000

    mouse speed home

--- dotfiles/config/autostart/mouse.sh
+++ dotfiles/config/autostart/mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {%@@ if profile=="home" @@%}
-xinput --set-prop 13 'libinput Accel Speed' -.75
+xinput --set-prop 15 'libinput Accel Speed' -.75
 {%@@ elif profile=="laptop" @@%}
 setxkbmap -option caps:escape
 {%@@ endif @@%}
