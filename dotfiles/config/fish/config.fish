589b3934a55f184e607ff73b71b343e62727ea02
cjber <cjberragan@gmail.com>
2020-04-03 10:51:07 +0100

    update ranger function

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -10,7 +10,6 @@ set fish_cursor_visual      block
 
 # alias
 alias v="nvim"
-alias lf="source ranger"
 alias vinit="nvim ~/.config/nvim/init.vim"
 alias r="radian"
 alias la="ncdu"
@@ -45,3 +44,27 @@ status --is-interactive; and pyenv virtualenv-init - | source
 #vim wiki
 alias vw="nvim ~/drive/wiki/index.wiki"
 
+
+function ranger-cd {
+    # create a temp file and store the name
+    tempfile="$(mktemp -t tmp.XXXXXX)"
+
+    # run ranger and ask it to output the last path into the
+    # temp file
+    ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+
+    # if the temp file exists read and the content of the temp
+    # file was not equal to the current path
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        # change directory to the path in the temp file
+        cd -- "$(cat "$tempfile")"
+    fi
+
+    # its not super necessary to have this line for deleting
+    # the temp file since Linux should handle it on the next
+    # boot
+    rm -f -- "$tempfile"
+}
+
+alias ls=ranger-cd
