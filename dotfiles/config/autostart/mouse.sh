19bc8ac31b530c264f51bcf85808b0f0b62538cf
cjber <cjberragan@gmail.com>
2020-04-03 09:12:44 +0100

    specify mouse name for speed

--- dotfiles/config/autostart/mouse.sh
+++ dotfiles/config/autostart/mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {%@@ if profile=="home" @@%}
-xinput --set-prop 15 'libinput Accel Speed' -.75
+xinput --set-prop 'pointer:Razer Razer Abyssus V2' 'libinput Accel Speed' -.75
 {%@@ elif profile=="laptop" @@%}
 setxkbmap -option caps:escape
 {%@@ endif @@%}
