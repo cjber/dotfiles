ba2c42fdfb003667f911018f0b1956be32b88fd5
cjber <cjberragan@gmail.com>
2020-04-21 15:05:01 +0100

    daily

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -46,3 +46,5 @@ status --is-interactive; and pyenv virtualenv-init - | source
 alias vw="nvim ~/drive/wiki/index.md"
 
 alias enc="encfs ~/drive/data_enc ~/data"
+
+set PATH /home/cjber/.gem/ruby/2.7.0/bin $PATH
