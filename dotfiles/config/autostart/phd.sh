b333f947440d656bc20c6f5d029abf2e1d7fa803
cjber <cjberragan@gmail.com>
2020-05-12 12:30:06 +0100

    changed: dotfiles/config/autostart/dots.sh dotfiles/config/autostart/phd.sh dotfiles/config/nvim/init.vim

--- dotfiles/config/autostart/phd.sh
+++ dotfiles/config/autostart/phd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ~/drive/phd/modules
 git add .
-git commit -m "`git show --pretty="" --name-only`"
+git commit -m "changed: `git show --pretty="" --name-only`"
 git push origin master
 
 cd ~/drive/phd/literature/
 git add .
-git commit -m "`git show --pretty="" --name-only`"
+git commit -m "changed: `git show --pretty="" --name-only`"
 git push origin master
