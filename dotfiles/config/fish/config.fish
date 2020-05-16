86d0d7b656edb9f637948307aedb772e2fcd0066
cjber <cjberragan@gmail.com>
2020-05-16 17:30:31 +0000

    kitty fonts

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -17,7 +17,7 @@ alias vinit="nvim ~/.config/nvim/init.vim"
 alias r="radian"
 alias la="ncdu"
 alias rm="trashf"
-alias empty='echo -n Emptying the Bin | pv -qL 10 && command rm -rf ~/.local/share/Trash/files/*'
+alias empty='sudo echo -n Emptying the Bin | pv -qL 10 && command rm -rf ~/.local/share/Trash/files/*'
 alias conf="cd ~/dotfiles/dotfiles/"
 
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
