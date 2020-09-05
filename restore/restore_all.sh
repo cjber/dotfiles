34ab884a40893c15b9ad0e6897dcd5c3c36433ff
cjber <cjberragan@gmail.com>
2020-09-05 22:53:52 +0100

    update install script

--- restore/restore_all.sh
+++ restore/restore_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git clone https://aur.archlinux.org/yay.git && \
     cd yay && \
+    makepkg -si && \
     sudo pacman -U yay*.pkg.* --noconfirm && \
     cd && \
     rm -rf yay
-yay -S --needed --noconfirm - < applist
+#yay -S --needed --noconfirm - < applist
