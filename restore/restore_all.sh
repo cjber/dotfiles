a7f6fb0ed5a48269b174addbf664574a7e78c02f
cjber <cjberragan@gmail.com>
2020-09-29 13:02:44 +0100

    additions for arch

--- restore/restore_all.sh
+++ restore/restore_all.sh
@@ -6,3 +6,24 @@ git clone https://aur.archlinux.org/yay.git && \
     cd && \
     rm -rf yay
 yay -S --needed --noconfirm - < applist
+
+sudo systemctl enable ly.service
+sudo chsh cjber
+
+pyenv install 3.8.5
+pyenv virtualenv 3.8.5 py3nvim
+pyenv activate py3nvim
+pip install neovim \
+    wheel \
+    python-language-server[all] \
+    seaborn \
+    matplotlib \
+    pandas \
+    numpy \
+    statsmodels \
+    allennlp \
+    allennlp-models
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall +qall
