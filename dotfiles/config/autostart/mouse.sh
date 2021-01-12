c4583bbc501f1c1a37fb3e38816eb5d0d63c7184
cjber <cjberragan@gmail.com>
2021-01-12 16:44:16 +0000

    ignore vim pack etc

--- dotfiles/config/autostart/mouse.sh
+++ dotfiles/config/autostart/mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {%@@ if profile=="home" @@%}
-xinput --set-prop 'pointer:Razer Razer Abyssus V2' 'libinput Accel Speed' -.75
+xinput --set-prop 'pointer:Logitech G903 LS' 'libinput Accel Speed' -.75
 {%@@ elif profile=="laptop" @@%}
 setxkbmap -option caps:escape
 {%@@ endif @@%}
