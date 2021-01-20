5fd0ca351f9f4c0d040013ae12743c63ee4cb326
cjber <cjberragan@gmail.com>
2021-01-20 15:21:50 +0000

    prepare for install

--- restore/02_restore_all.sh
+++ restore/02_restore_all.sh
@@ -2,14 +2,16 @@
 git clone https://aur.archlinux.org/yay.git \
         && cd yay \
         && makepkg -sri --needed --noconfirm \
-        && cd \
-        && rm -rf .cache yay
-yay -S --needed --noconfirm - < applist
+        && cd .. \
+        && rm -rf yay
+yay -Rc go --noconfirm \
+    && yes | yay -Scc \
+    && yay -S --needed --noconfirm - < applist
 
-pyenv install 3.8.6
-pyenv virtualenv 3.8.6 py3nvim
-pyenv activate py3nvim
-pip install \
-    wheel \
-    neovim
+# pyenv install 3.8.6
+# pyenv virtualenv 3.8.6 py3nvim
+# pyenv activate py3nvim
+# pip install \
+#     wheel \
+#     neovim
 #dotdrop --profile= install
