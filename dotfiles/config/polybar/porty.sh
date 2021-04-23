fe05099e9ff4c3fa35985b0a896bb3770f5e1447
cjber <cjberragan@gmail.com>
2021-04-23 12:48:54 +0100

    porty

---
+++ dotfiles/config/polybar/porty.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+NANO=$(curl -s rate.sx/683nano)
+BUX=$(curl -s rate.sx/1232bux)
+BAT=$(curl -s rate.sx/257bat)
+BAN=$(curl -s rate.sx/20298ban)
+OMG=$(curl -s rate.sx/32omg)
+
+echo $NANO + $BUX + $BAT + $BAN + $OMG | bc
