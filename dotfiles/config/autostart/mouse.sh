962a769a003b08a18d98a5601063b68fe42bc9d2
cjber <cjberragan@gmail.com>
2021-09-23 12:28:43 +0100

    add autostarts

--- dotfiles/config/autostart/mouse.sh
+++ dotfiles/config/autostart/mouse.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 {%@@ if profile=="home" @@%}
+hsetroot -solid "#1a1b26"
+xset r rate 500 60
+xrandr --output DP-2 --mode 3440x1440 --rate 144
 xinput --set-prop 'pointer:Logitech G903 LS' 'libinput Accel Speed' -.70
 {%@@ elif profile=="laptop" @@%}
 setxkbmap -option caps:escape
