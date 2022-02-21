b06fcf87014d082f17063aa408fb7d7a5aae3ed3
cjber <cjberragan@gmail.com>
2022-02-21 17:36:02 +0000

    add UK spell file

--- restore/01_restore_all_root.sh
+++ restore/01_restore_all_root.sh
@@ -2,5 +2,6 @@
 sed -i "s,^#MAKEFLAGS=.*,MAKEFLAGS='-j$(nproc)',g" /etc/makepkg.conf \
         && sed -i "s,^PKGEXT=.*,PKGEXT='.pkg.tar',g" /etc/makepkg.conf \
         && sed -i "s,^#Color,Color,g" /etc/makepkg.conf
-pacman -Syu --needed --noconfirm git reflector zsh
+pacman -Syu --needed --noconfirm git reflector zsh aspell aspell-en
 reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+aspell -d en_GB dump master | aspell -l en expand > /usr/share/dict/gb_english.dic
