fecec3574d73d3284501f08ceafcdb31b00c7d87
cjber <cjberragan@gmail.com>
2022-03-29 13:49:17 +0100

    fix cursor

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
-# Terminate already running bar instances
 killall -q polybar
 
+
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top
-
-echo "Bars launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
