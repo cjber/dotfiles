6c83d3fc5ad44ea8e247045ee1065ae963ab3ed1
cjber <cjberragan@gmail.com>
2022-06-07 16:43:52 +0100

    update packer install

--- dotfiles/config/nvim/lua/plugins/init.lua
+++ dotfiles/config/nvim/lua/plugins/init.lua
@@ -1,18 +1,3 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({
-		"git",
-		"clone",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	execute("packadd packer.nvim")
-end
-
 require("plugins.plugs") -- load plugins
 require("plugins.treesitter") -- syntax
 require("plugins.telescope") -- interactive search
