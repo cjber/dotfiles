819bcdd5606a3f48fbf20abf9aa4758200ddebdc
cjber <cjberragan@gmail.com>
2022-06-09 17:14:52 +0100

    update polybar script

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -5,8 +5,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-MONITOR=DP-2 polybar --reload top &
-
-# for m in $(polybar --list-monitors | cut -d":" -f1); do
-#     MONITOR=$m polybar --reload top &
-# done
+MONITOR=$(polybar --list-monitors | cut -d":" -f1 | head -n 1) polybar --reload top &
