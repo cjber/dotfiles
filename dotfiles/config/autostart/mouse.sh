6e907261b286a2bf8c190425d143ef9a160acc19
cjber <cjberragan@gmail.com>
2023-02-08 10:10:53 +0000

    add antidote zsh

--- dotfiles/config/autostart/mouse.sh
+++ dotfiles/config/autostart/mouse.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 {%@@ if profile=="home" @@%}
-hsetroot -solid "#1a1b26"
 xset r rate 500 60
 xinput --set-prop 'pointer:Logitech G903 LS' 'libinput Accel Speed' -.70
+hsetroot -solid "#1a1b26"
+hsetroot -solid "#1a1b26"
 {%@@ elif profile=="laptop" @@%}
 setxkbmap -option caps:escape
+hsetroot -solid "#1a1b26"
 {%@@ endif @@%}
 
