3703ce42f925935ff7c76a7a3a2948503ca2f6a0
cjber <cjberragan@gmail.com>
2023-02-14 14:34:27 +0000

    add joshuto

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,13 +1,19 @@
 local cmd = vim.cmd
 local g = vim.g
 local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-if fn.empty(fn.glob(install_path)) > 0 then
-	PackerBootstrap =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-	vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
 require("settings")
 require("plugins")
--- dotfiles/config/nvim/lua/plugins/plugs.lua
+++ dotfiles/config/nvim/lua/plugins/plugs.lua
@@ -1,42 +1,36 @@
-vim.cmd([[packadd packer.nvim]])
-
-require("packer").init({
-	display = { compact = true },
-})
-
-return require("packer").startup(function()
-	use({ "wbthomason/packer.nvim" })
-	use({ "noib3/nvim-cokeline" })
-	use({ "nvim-lualine/lualine.nvim" })
-	use({ "ray-x/lsp_signature.nvim" })
-	use({ "direnv/direnv.vim" })
-	use({
+local plugins = {
+	{ "wbthomason/packer.nvim" },
+	{ "noib3/nvim-cokeline" },
+	{ "nvim-lualine/lualine.nvim" },
+	{ "ray-x/lsp_signature.nvim" },
+	{ "direnv/direnv.vim" },
+	{
 		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use({ "goolord/alpha-nvim" })
+		req = { { "nvim-lua/plenary.nvim" } },
+	},
+	{ "goolord/alpha-nvim" },
 
-	use({ "quarto-dev/quarto-nvim", requires = { "jmbuhr/otter.nvim" } })
-	use({ "kyazdani42/nvim-web-devicons" })
-	use({ "kyazdani42/nvim-tree.lua", tag = "nightly" })
+	{ "quarto-dev/quarto-nvim", dependencies = { "jmbuhr/otter.nvim" } },
+	{ "kyazdani42/nvim-web-devicons" },
+	{ "kyazdani42/nvim-tree.lua", tag = "nightly" },
 
-	use({ "ahmedkhalf/project.nvim" }) -- projects
-	use({ "lukas-reineke/headlines.nvim" }) -- add header highlights for md qmd etc
+	{ "ahmedkhalf/project.nvim" }, -- projects
+	{ "lukas-reineke/headlines.nvim" }, -- add header highlights for md qmd etc
 
-	use({ "lewis6991/impatient.nvim" }) -- faster loading
-	use({ "vim-pandoc/vim-pandoc-syntax" })
-	use({ "quarto-dev/quarto-vim" })
-	use({ "Pocco81/auto-save.nvim" }) -- autosave
-	-- use({ "Konfekt/FastFold" }) -- better folds
-	use({ "numToStr/Comment.nvim" })
-	use({ "folke/trouble.nvim" }) -- better lsp error search
-	use({ "folke/neodev.nvim" }) -- lua dev stuff
-	use({ "folke/todo-comments.nvim" })
-	use({ "folke/tokyonight.nvim" }) -- theme
-	use({ "folke/which-key.nvim" }) -- visualise bindings
-	use({ -- auto completions
+	{ "lewis6991/impatient.nvim" }, -- faster loading
+	{ "vim-pandoc/vim-pandoc-syntax" },
+	{ "quarto-dev/quarto-vim" },
+	{ "Pocco81/auto-save.nvim" }, -- autosave
+	-- { "Konfekt/FastFold" }, -- better folds
+	{ "numToStr/Comment.nvim" },
+	{ "folke/trouble.nvim" }, -- better lsp error search
+	{ "folke/neodev.nvim" }, -- lua dev stuff
+	{ "folke/todo-comments.nvim" },
+	{ "folke/tokyonight.nvim" }, -- theme
+	{ "folke/which-key.nvim" }, -- visualise bindings
+	{ -- auto completions
 		"hrsh7th/nvim-cmp",
-		requires = {
+		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
@@ -46,42 +40,38 @@ return require("packer").startup(function()
 			"hrsh7th/vim-vsnip-integ",
 			"hrsh7th/cmp-cmdline",
 			"lukas-reineke/cmp-under-comparator",
-			-- { "tzachar/cmp-tabnine", run = "./install.sh" },
 		},
-	})
-	use({ "jalvesaq/vimcmdline" }) -- repl
-	use({ "danymat/neogen" })
-	use({ "lervag/vimtex" }) -- latex tools
-	use({ "lukas-reineke/indent-blankline.nvim" }) -- indent guide
-	use({ "neovim/nvim-lspconfig" }) -- language servers
-	use({ "norcalli/nvim-colorizer.lua" }) -- highlight colours
-	use({ "nvim-lua/plenary.nvim" })
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- syntax highlight
-	use({ "nvim-treesitter/nvim-treesitter-textobjects" }) -- treesitter movements
-	use({ "onsails/lspkind-nvim" }) -- lsp symbols
-	use({ "simrat39/symbols-outline.nvim" })
-	use({ "mbbill/undotree" }) -- undo tree
-	use({ "tpope/vim-dispatch" }) -- dispatch commands
-	use({ "tpope/vim-repeat" }) -- repeat plugin cmds (including lightspeed)
-	use({ "kylechui/nvim-surround" }) -- change around brackets etc
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }) -- better folding
-	use({ "tweekmonster/startuptime.vim" }) -- benchmark nvim startup
-	use({ "windwp/nvim-autopairs" }) -- pairs
-	use({ "rafamadriz/friendly-snippets" }) -- default snippets
-	use({ "numToStr/FTerm.nvim" }) -- floating terminal
-	use({ "petertriho/nvim-scrollbar" }) -- scrollbar with diagnostics
-	use({ "kevinhwang91/nvim-hlslens" }) -- hl matches
-	use({ "kazhala/close-buffers.nvim" }) -- buffer close commands
-	use({ "jose-elias-alvarez/null-ls.nvim" }) -- other lsp
-	use({ "LostNeophyte/null-ls-embedded" })
-	-- use({ "j-hui/fidget.nvim" }) -- lsp progress
-	use({ "dbeniamine/todo.txt-vim" }) -- todo.txt highlighting
-	use({ "simrat39/rust-tools.nvim" })
-	use({ "saecki/crates.nvim" })
-	use({ "phaazon/hop.nvim" })
-	use({ "barreiroleo/ltex_extra.nvim" })
+	},
+	{ "jalvesaq/vimcmdline" }, -- repl
+	{ "danymat/neogen" },
+	{ "lervag/vimtex" }, -- latex tools
+	{ "lukas-reineke/indent-blankline.nvim" }, -- indent guide
+	{ "neovim/nvim-lspconfig" }, -- language servers
+	{ "norcalli/nvim-colorizer.lua" }, -- highlight colours
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }, -- syntax highlight
+	{ "nvim-treesitter/nvim-treesitter-textobjects" }, -- treesitter movements
+	{ "onsails/lspkind-nvim" }, -- lsp symbols
+	{ "simrat39/symbols-outline.nvim" },
+	{ "mbbill/undotree" }, -- undo tree
+	{ "tpope/vim-dispatch" }, -- dispatch commands
+	{ "tpope/vim-repeat" }, -- repeat plugin cmds (including lightspeed)
+	{ "kylechui/nvim-surround" }, -- change around brackets etc
+	{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" }, -- better folding
+	{ "tweekmonster/startuptime.vim" }, -- benchmark nvim startup
+	{ "windwp/nvim-autopairs" }, -- pairs
+	{ "rafamadriz/friendly-snippets" }, -- default snippets
+	{ "numToStr/FTerm.nvim" }, -- floating terminal
+	{ "petertriho/nvim-scrollbar" }, -- scrollbar with diagnostics
+	{ "kevinhwang91/nvim-hlslens" }, -- hl matches
+	{ "kazhala/close-buffers.nvim" }, -- buffer close commands
+	{ "jose-elias-alvarez/null-ls.nvim" }, -- other lsp
+	{ "LostNeophyte/null-ls-embedded" },
+	{ "dbeniamine/todo.txt-vim" }, -- todo.txt highlighting
+	{ "simrat39/rust-tools.nvim" },
+	{ "saecki/crates.nvim" },
+	{ "phaazon/hop.nvim" },
+	{ "barreiroleo/ltex_extra.nvim" },
+}
 
-	if PackerBootstrap then
-		require("packer").sync()
-	end
-end)
+require("lazy").setup(plugins)
