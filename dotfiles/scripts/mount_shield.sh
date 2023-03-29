bf178a949e93f9237b14b88a042845c4db2c7380
cjber <cjberragan@gmail.com>
2023-03-29 16:08:23 +0100

    update

--- dotfiles/scripts/mount_shield.sh
+++ dotfiles/scripts/mount_shield.sh
@@ -1,1 +1,1 @@
-sudo mount -t cifs //192.168.0.210/External /mnt -o username=cjberragan,password=slit-qqq-egg,uid=cjber
+sudo mount -t cifs //192.168.0.209/External /mnt -o username=cjberragan,password=slit-qqq-egg,uid=cjber
