2b7bf579b2c5e8258e945b62481eab987ec4388d
cjber <cjberragan@gmail.com>
2023-05-23 10:36:00 +0100

    small

--- dotfiles/config/nvim/lua/plugins/plugs.lua
+++ dotfiles/config/nvim/lua/plugins/plugs.lua
@@ -22,7 +22,48 @@ local plugins = {
 			-- Autocompletion
 			{
 				"hrsh7th/nvim-cmp",
-				opts = require("plugins.cmp"),
+				opts = function()
+					local cmp = require("cmp")
+					return {
+						completion = { keyword_length = 1 },
+
+						window = {
+							completion = {
+								border = "single",
+								winhighlight = "NormalFloat:NormalFloat,Title:NormalFloat",
+								col_offset = -3,
+								side_padding = 0,
+							},
+							documentation = {
+								border = "single",
+								winhighlight = "NormalFloat:NormalFloat,Title:NormalFloat",
+							},
+						},
+						mapping = cmp.mapping.preset.insert({
+							["<C-b>"] = cmp.mapping.scroll_docs(-4),
+							["<C-f>"] = cmp.mapping.scroll_docs(4),
+							["<C-Space>"] = cmp.mapping.complete(),
+							["<C-e>"] = cmp.mapping.abort(),
+							["<CR>"] = cmp.mapping.confirm({ select = true }),
+						}),
+						sources = {
+							{ name = "nvim_lsp" },
+							{ name = "otter" },
+							{ name = "luasnip" },
+							{ name = "kitty", option = { listen_on = vim.env.KITTY_LISTEN_ON } },
+							{ name = "nvim_lua" },
+							{ name = "buffer" },
+							{
+								name = "path",
+								option = {
+									get_cwd = function()
+										return vim.fn.expand(vim.fn.getcwd())
+									end,
+								},
+							},
+						},
+					}
+				end,
 			},
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
