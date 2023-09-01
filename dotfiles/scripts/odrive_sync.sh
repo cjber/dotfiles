b37ed6ca44c206539c8d858eeebecb5381e3b8df
cjber <cjberragan@gmail.com>
2023-09-01 16:23:35 +0100

    add crontab file

--- dotfiles/scripts/odrive_sync.sh
+++ dotfiles/scripts/odrive_sync.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-DRIVE=$HOME/drive
+LOCAL_DRIVE=$HOME/drive
+REMOTE_DRIVE=odrive:drive
+
 BACKUP=odrive:backup
 EXCLUDE_FILE=$HOME/scripts/exclude.txt
-LOG_FILE=$HOME/scripts/odrive_sync.log
+LOG_FILE=$HOME/odrive_sync.log
 
 if pgrep -fl rclone; then exit 1; fi
 
-/usr/bin/rclone sync "$DRIVE" odrive:drive \
+/usr/bin/rclone sync "$LOCAL_DRIVE" $REMOTE_DRIVE \
     --max-age 24h \
-    --no-traverse \
     --backup-dir="$BACKUP" \
     --suffix ."$(date +"%Y-%m-%d-%H-%M-%S")" \
     --suffix-keep-extension \
