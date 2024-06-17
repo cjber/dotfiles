489aad521bc2321f9b7847d75dfd8a78bc315a47
cjber <cjberragan@gmail.com>
2024-06-17 15:56:35 +0100

    move back to p10k as it's faster

--- dot_zsh_plugins.zsh
+++ dot_zsh_plugins.zsh
@@ -1,7 +1,5 @@
 fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
-source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
 fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
@@ -10,6 +8,3 @@ fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-
 source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-twang817-SLASH-zsh-manydots-magic )
 source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-twang817-SLASH-zsh-manydots-magic/zsh-manydots-magic.plugin.zsh
-fpath+=( /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
-source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
-source /home/cjber/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
