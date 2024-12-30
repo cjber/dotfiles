df360ce201d9ad306c82204a3870cde58bb454cc
cjber <cjberragan@gmail.com>
2024-12-30 15:02:27 +0000

    update nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -59,29 +59,29 @@ return {
     end,
   },
 
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
-    opts = {
-      provider = "openai",
-    },
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "HakonHarnes/img-clip.nvim",
-      --- The below is optional, make sure to setup it properly if you have lazy=true
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+  --   opts = {
+  --     provider = "openai",
+  --   },
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     "HakonHarnes/img-clip.nvim",
+  --     --- The below is optional, make sure to setup it properly if you have lazy=true
+  --     {
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 
   { "freitass/todo.txt-vim", lazy = false },
   {
