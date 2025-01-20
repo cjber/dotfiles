49a73224cf846053c23645bff212f1be163e7733
cjber <cjberragan@gmail.com>
2025-01-20 12:23:09 +0000

    style: simplify gdrive sync

--- scripts/executable_gdrive_sync.sh
+++ scripts/executable_gdrive_sync.sh
@@ -2,40 +2,24 @@
 
 LOCAL_DRIVE=$HOME/drive
 REMOTE_DRIVE=gdrive:drive
-BACKUP=gdrive:backup
 
-LOCAL_DATA=$HOME/data
-REMOTE_DATA=gdrive:data
-
-BACKUP_DATA=gdrive:backup_data
 EXCLUDE_FILE=$HOME/scripts/exclude.txt
-LOG_FILE=$HOME/.cache/gdrive_sync.log
 
-LOCAL_TODO=$HOME/drive/todo
-REMOTE_TODO=gdrive:todo
+BACKUP_TIMESTAMP=$(date +"%Y-%m-%d")
+BACKUP_DIR="gdrive:backup/$BACKUP_TIMESTAMP"
 
 if pgrep -fl rclone; then exit 1; fi
 
 /usr/bin/rclone sync "$LOCAL_DRIVE" $REMOTE_DRIVE \
-    --exclude-from "$EXCLUDE_FILE" \
-    --backup-dir="$BACKUP" \
-    --suffix ."$(date +"%Y-%m-%d-%H-%M-%S")" \
-    --suffix-keep-extension \
+    --filter-from "$EXCLUDE_FILE" \
+    --delete-excluded \
     --fast-list \
+    --transfers 16 \
+    --checkers 16 \
+    --drive-chunk-size 64M \
     --progress \
     --verbose \
-    --log-file="$LOG_FILE" \
-    --max-age "$1"
-
-/usr/bin/rclone sync "$LOCAL_DATA" $REMOTE_DATA \
-    --exclude-from "$EXCLUDE_FILE" \
-    --backup-dir="$BACKUP_DATA" \
+    --backup-dir $BACKUP_DIR \
     --suffix ."$(date +"%Y-%m-%d-%H-%M-%S")" \
     --suffix-keep-extension \
-    --fast-list \
-    --progress \
-    --verbose \
-    --log-file="$LOG_FILE" \
-    --max-age "$1"
-
-/usr/bin/rclone bisync "$LOCAL_TODO" $REMOTE_TODO --resync
+    --max-age 7d
