eee5e732eb06c51aa4acd01253b7d748cdfd2523
cjber <cjberragan@gmail.com>
2025-03-16 12:28:56 +0000

    refactor(nvim): Reorganize key mappings and plugin configuration

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -3,7 +3,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local nomap = vim.keymap.del
 
--- unmap some defaults
+-- Remove default mappings
 nomap("n", "<leader>e")
 nomap("n", "<leader>h")
 nomap("n", "<leader>v")
@@ -15,29 +15,39 @@ nomap("n", "<leader>fm")
 nomap("n", "<leader>cm")
 nomap("n", "<leader>ch")
 
--- normal mappings
+-- Navigation
 map("n", "j", "gj")
 map("n", "k", "gk")
-map("n", "q:", "<CMD>q<CR>", { desc = "Quit" })
-map("n", "M", "<CMD>join<CR>", { desc = "Join lines" })
+
 map("n", "J", "}", { desc = "Next paragraph" })
 map("n", "K", "{", { desc = "Previous paragraph" })
 map("n", "H", "^", { desc = "Start of line" })
 map("n", "L", "$", { desc = "End of line" })
-map("n", "U", "<C-r>", { desc = "Redo" })
+
+map("v", "J", "}", { desc = "Next paragraph" })
+map("v", "K", "{", { desc = "Previous paragraph" })
+map("v", "H", "^", { desc = "Start of line" })
+map("v", "L", "$", { desc = "End of line" })
+
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Editing
+map("n", "M", "<CMD>join<CR>", { desc = "Join lines" })
+map("n", "U", "<C-r>", { desc = "Redo" })
+map("n", "q:", "<CMD>q<CR>", { desc = "Quit" })
+map("v", "M", "J", { desc = "Join lines" })
+
+map("v", "<", "<gv", { desc = "Indent left and reselect" })
+map("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Window Management
 map("n", "<Up>", "<CMD>resize +2<CR>", { desc = "Increase height" })
 map("n", "<Down>", "<CMD>resize -2<CR>", { desc = "Decrease height" })
-map("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open Oil" })
 
--- spell
-map("n", "<leader>ss", "<CMD>set invspell<CR>", { desc = "Toggle spell check" })
-map("n", "<leader>sf", "ma[s1z=`a", { desc = "Fix spelling" })
-
--- buffers
+-- Buffer Management
 map(
   "n",
   "<leader>bo",
@@ -51,24 +61,7 @@ map(
   { desc = "Close current buffer" }
 )
 
--- update
-map("n", "<leader>u", "<CMD>Lazy sync<CR>", { desc = "Lazy Update" })
-
--- hop
-map("n", "s", "<CMD>HopWord<CR>", { desc = "Hop to word" })
-map("n", "S", "<CMD>HopLineStart<CR>", { desc = "Hop to line start" })
-
--- visual mappings
-map("v", "M", "J", { desc = "Join lines" })
-map("v", "J", "}", { desc = "Next paragraph" })
-map("v", "K", "{", { desc = "Previous paragraph" })
-map("v", "<", "<gv", { desc = "Indent left and reselect" })
-map("v", ">", ">gv", { desc = "Indent right and reselect" })
-
--- terminal mappings
-map("t", "<ESC><ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- LSP mappings
+-- LSP
 map("n", "<leader>le", "<CMD>Trouble diagnostics toggle<CR>", { desc = "Toggle diagnostics" })
 map("n", "<leader>lf", "<CMD>lua require'conform'.format()<CR>", { desc = "Format" })
 map("n", "<leader>la", "<CMD>lua vim.lsp.buf.code_action()<CR>", { desc = "Code action" })
@@ -83,15 +76,27 @@ map(
   "<CMD>lua require('telescope.builtin').lsp_document_symbols({symbols={'method', 'function', 'class'}})<CR>",
   { desc = "LSP symbols" }
 )
-map("n", "<leader>fl", "<CMD>Telescope file_browser<CR>", { desc = "File Browser" })
 map("n", "<leader>ll", "<CMD>Trouble lsp_document_symbols toggle<CR>", { desc = "LSP symbols" })
 map("n", "<leader>ld", "<CMD>Trouble lsp_references toggle<CR>", { desc = "LSP references" })
 
--- avante
+-- Tools
+map("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open Oil" })
+map("n", "<leader>fl", "<CMD>Telescope file_browser<CR>", { desc = "File Browser" })
+map("n", "<leader>fu", "<CMD>UndotreeToggle<CR>", { desc = "Undo" })
+map("n", "<leader>gg", "<CMD>Neogit<CR>", { desc = "Neogit" })
+map("n", "<leader>mp", "<CMD>PeekOpen<CR>", { desc = "Open Peek" })
 map("n", "<leader>aa", "<CMD>AvanteToggle<CR>", { desc = "Avante" })
 
--- undo
-map("n", "<leader>fu", "<CMD>UndotreeToggle<CR>", { desc = "Undo" })
+-- Spell
+map("n", "<leader>ss", "<CMD>set invspell<CR>", { desc = "Toggle spell check" })
+map("n", "<leader>sf", "ma[s1z=`a", { desc = "Fix spelling" })
 
--- neogit
-map("n", "<leader>gg", "<CMD>Neogit<CR>", { desc = "Neogit" })
+-- Hop
+map("n", "s", "<CMD>HopWord<CR>", { desc = "Hop to word" })
+map("n", "S", "<CMD>HopLineStart<CR>", { desc = "Hop to line start" })
+
+-- Terminal
+map("t", "<ESC><ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Updates
+map("n", "<leader>u", "<CMD>Lazy sync<CR>", { desc = "Lazy Update" })
