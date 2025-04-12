704f9817c92ef6f4b9d4bc5c6608484cbae9f4bd
cjber <cjberragan@gmail.com>
2025-04-12 21:54:36 +0100

    feat(sync):

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { "isort", "black", "injected" },
+      python = { "ruff_fix", "ruff_format", "ruff_organize_imports", "injected" },
       sql = { "sqlfmt" },
       lua = { "stylua" },
       javascript = { "prettier" },
